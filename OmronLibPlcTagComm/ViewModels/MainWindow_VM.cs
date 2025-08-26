1f6523f0b4ed999eca517fcaa35947b05a983b1f
Samet CETIN <samet.cetin@haffner.com.tr>
2025-08-26 10:23:35 +0300

    write and read test with dynamic value

--- OmronLibPlcTagComm/ViewModels/MainWindow_VM.cs
+++ OmronLibPlcTagComm/ViewModels/MainWindow_VM.cs
@@ -12,7 +12,6 @@ public class MainWindow_VM
 {
 	private readonly IMessageService _messageService;
 
-	public IRelayCommand ConnectCommand { get; }
 	public IRelayCommand ReadPlcVarCommand { get; }
 	public IRelayCommand WritePlcVarCommand { get; }
 
@@ -24,7 +23,6 @@ public class MainWindow_VM
 	public MainWindow_VM(IMessageService messageService)
 	{
 		_messageService = messageService;
-		ConnectCommand = new RelayCommand(Connect);
 		ReadPlcVarCommand = new RelayCommand(ReadVar);
 		WritePlcVarCommand = new RelayCommand(WriteVar);
 
@@ -34,68 +32,121 @@ public class MainWindow_VM
 	public string StatusText { get; set; } = "Not connected";
 	public string PlcIpAddress { get; set; } = "192.168.250.1";
 	public int CounterValue { get; set; } = 0;
+
 	public string PlcVarName { get; set; } = "DB.HMI.AxisX_SetPosition";
+	public string PlcVariableType { get; set; } = "INT";
+	public List<string> PlcVariableTypes { get; } =
+	[
+		"BOOL", "INT", "UINT", "REAL", "LREAL", "STRING"
+	];
+
 	public string ValueReadFromPlc { get; set; } = "Empty";
 	public string ValueWriteToPlc { get; set; } = "Empty";
 
-	private void Connect()
-	{
-		PlcIpAddress = "192.168.250.3";
-		_messageService.ShowMessage(PlcIpAddress);
-	}
 
 
-	private readonly object _lockRead = new();
 	private void ReadVar()
 	{
-		if (string.IsNullOrEmpty(PlcVarName) || string.IsNullOrEmpty(PlcIpAddress))
+		if (string.IsNullOrEmpty(PlcVarName) || string.IsNullOrEmpty(PlcIpAddress) || string.IsNullOrEmpty(PlcVariableType))
 		{
 			return;
 		}
 
-		lock (_lockRead)
+		var res = ReadPlcVar(PlcVarName, PlcVariableType);
+		if (res.success)
 		{
-			var res = ReadPlcVar<float>(PlcVarName);
 			ValueReadFromPlc = res.value.ToString() ?? "Null"; // Ensure null safety by providing a fallback value
 		}
+		else
+			ValueReadFromPlc = "Read Error";
 	}
 
-	private (bool success, T? value) ReadPlcVar<T>(string varName) where T : struct
+	// plctag wrapper
+	private static dynamic ReadAny(string plcVarType, Tag tag)
 	{
-		Tag? plcTag = null;
-		try
+		object value = plcVarType switch
 		{
-			plcTag = new Tag
-			{
-				Name = PlcVarName,
-				Gateway = PlcIpAddress,
-				Path = "1,0",
-				PlcType = libplctag.PlcType.Omron,
-				Protocol = libplctag.Protocol.ab_eip,
-				Timeout = TimeSpan.FromMilliseconds(500)
-			};
+			var t when plcVarType == "BOOL" => tag.GetBit(0),
+			var t when plcVarType == "INT" => tag.GetInt16(0),
+			var t when plcVarType == "UINT" => tag.GetUInt16(0),
+			var t when plcVarType == "REAL" => tag.GetFloat32(0),
+			var t when plcVarType == "LREAL" => tag.GetFloat64(0),
+			var t when plcVarType == "STRING" => tag.GetString(0),
+			_ => throw new NotSupportedException($"Type {plcVarType} not supported")
+		};
 
-			plcTag.Read();
+		return value;
+	}
 
-			if (typeof(T) == typeof(float))
+
+	// Generic read method
+	private readonly object _lockRead = new();
+	private (bool success, dynamic value) ReadPlcVar(string varName, string varType)
+	{
+		lock (_lockRead)
+		{
+			Tag? plcTag = null;
+			try
 			{
-				var floatValue = plcTag.GetFloat32(0);
-				return (true, (T?)(object)floatValue);
+				plcTag = new Tag
+				{
+					Name = varName,
+					Gateway = PlcIpAddress,
+					Path = "1,0",
+					PlcType = PlcType.Omron,
+					Protocol = Protocol.ab_eip,
+					Timeout = TimeSpan.FromMilliseconds(500)
+				};
+
+				if (plcTag == null)
+					throw new Exception("Error occured when creating Tag");
+				if (!plcTag.IsInitialized)
+					plcTag.Initialize();
+				var value = ReadAny(varType, plcTag);
+				return (true, value);
 			}
-			else
+			catch (Exception ex)
 			{
+				StatusText = ex.Message;
 				return (false, null);
 			}
-		}
-		catch (Exception ex)
-		{
-			return (false, null);
-		}
-		finally
-		{
-			plcTag?.Dispose();
+			finally
+			{
+				plcTag?.Dispose();
+				plcTag = null;
+			}
 		}
 	}
+
+
+	private static void WriteAny(string plcVarType, Tag tag, dynamic value)
+	{
+
+		switch (plcVarType)
+		{
+			case "BOOL":
+				tag.SetBit(0, Convert.ToBoolean(value));
+				break;
+			case "INT":
+				tag.SetInt16(0, Convert.ToInt16(value));
+				break;
+			case "UINT":
+				tag.SetUInt16(0, Convert.ToUInt16(value));
+				break;
+			case "REAL":
+				tag.SetFloat32(0, Convert.ToSingle(value));
+				break;
+			case "LREAL":
+				tag.SetFloat64(0, Convert.ToDouble(value));
+				break;
+			case "STRING":
+				tag.SetString(0, value?.ToString() ?? "");
+				break;
+			default:
+				throw new NotSupportedException($"Type {plcVarType} not supported");
+		}
+	}
+
 
 	private readonly object _lockWrite = new();
 	private void WriteVar()
@@ -105,28 +156,38 @@ public class MainWindow_VM
 			return;
 		}
 
-		lock (_lockRead)
+		lock (_lockWrite)
 		{
+			Tag? plcTag = null;
+
 			try
 			{
-				testTagWr = new TagReal()
+				plcTag = new Tag
 				{
 					Name = PlcVarName,
 					Gateway = PlcIpAddress,
 					Path = "1,0",
-					PlcType = libplctag.PlcType.Omron,
-					Protocol = libplctag.Protocol.ab_eip,
+					PlcType = PlcType.Omron,
+					Protocol = Protocol.ab_eip,
 					Timeout = TimeSpan.FromMilliseconds(500)
 				};
 
-				if (float.TryParse(ValueWriteToPlc, out var valueToWrite))
-				{
-					testTagWr.Write(valueToWrite);
-				}
+				if (plcTag == null)
+					throw new Exception("Error occured when creating Tag");
+				if (!plcTag.IsInitialized)
+					plcTag.Initialize();
+
+				WriteAny(PlcVariableType, plcTag, ValueWriteToPlc);
+				plcTag.Write();
 			}
 			catch (Exception ex)
 			{
 				ValueWriteToPlc = ex.Message;
+			}
+			finally
+			{
+				plcTag?.Dispose();
+				plcTag = null;
 			}
 		}
 
